7076301b115f61a643d6553f69c6ee88ecaf1689
Minh Khoi Le <khoimle7@gmail.com>
2023-07-16 21:48:01 +0700

    update ffmpeg execution command don't recognize file path with space

--- main.py
+++ main.py
@@ -142,8 +142,7 @@ if check_ext("input.mp4"):
             "directory as input.")
         out_path = select_path("Select the output directory: ")
         print("\nExporting...")
-        os.system('ffmpeg.exe -v quiet -stats -i input.mp4 -c:v libx264 ' + '"' + out_path + convertname(
-            out_name) + '.mp4"')
+        os.system('ffmpeg.exe -v quiet -stats -i input.mp4 -c:v libx264 ' + f'"{out_path + convertname(out_name)}"' + '.mp4')
         print("\nFinished exporting.")
         flag = 1
     elif a == '2':
@@ -160,8 +159,7 @@ if check_ext("input.mp4"):
         end = float(input("Enter the end time to cut (seconds): "))
         os.system(
             'ffmpeg.exe -v quiet -stats -ss ' + str(begin) + ' -t ' + str(
-                end - begin) + ' -i input.mp4 -y -c:v libx264 -c copy ' + '"' + out_path + convertname(
-                out_name) + '.mp4"')
+                end - begin) + ' -i input.mp4 -y -c:v libx264 -c copy ' + f'{"out_path + convertname(out_name)"}' + '.mp4')
         print("\nFinished Exporting.")
         flag = 1
 
