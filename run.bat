98a2c81c7ee2dcc0a996a4c02d96ad44424ad607
nguyenanhducxmen <75881514+nguyenanhducxmen@users.noreply.github.com>
2023-07-16 23:06:49 +0700

    Check for python folder.

--- run.bat
+++ run.bat
@@ -1,24 +1,6 @@
-@echo off
-set /p ver=Enter version number:
-cls
-echo Installing PyInstaller...
-echo:
-pip install pyinstaller
-cls
-echo Building executable...
-echo:
-pyinstaller --onefile main.py
-cls
-echo Moving file to root destination...
-echo:
-cd dist
-move main.exe ..
-cd ..
-rmdir dist
-ren main.exe yt-dlp-cutter-%ver%.exe
-del *.spec /s /q /f
-rmdir build /s /f
-cls
-echo Complete building version %ver%.
-echo:
-pause
+@ECHO OFF
+IF EXIST "./python/python.exe" (
+    .\python\python.exe main.py
+) ELSE (
+    ECHO Python folder doesn't exist. Please extract them.
+)
