fd96c06f1ecd36b9e53ad27fd9459bbe60274c10
nguyenanhducxmen <75881514+nguyenanhducxmen@users.noreply.github.com>
2023-07-16 23:07:27 +0700

    Automatic version number renaming + file cleaning

--- build.bat
+++ build.bat
@@ -1,3 +1,24 @@
 @echo off
+set /p ver=Enter version number:
+cls
+echo Installing PyInstaller...
+echo:
 pip install pyinstaller
-pyinstaller main.py
+cls
+echo Building executable...
+echo:
+pyinstaller --onefile main.py
+cls
+echo Moving file to root destination...
+echo:
+cd dist
+move main.exe ..
+cd ..
+rmdir dist
+ren main.exe yt-dlp-cutter-%ver%.exe
+del *.spec /s /q /f
+rmdir build /s /f
+cls
+echo Complete building version %ver%.
+echo:
+pause
